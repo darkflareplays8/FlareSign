700cab70287a365c79a768af5c7a090bda5f1226
DarkFlare <saimuralibalmuri@gmail.com>
2026-03-09 21:25:11 +0000

    Update OTAInstallService.swift

--- services/OTAInstallService.swift
+++ services/OTAInstallService.swift
@@ -3,97 +3,136 @@ import UIKit
 
 class OTAInstallService {
     static let shared = OTAInstallService()
-    private let port = 8765
     private var server: LocalHTTPServer?
 
-    func install(ipaURL: URL, bundleID: String, appName: String) {
-        server?.stop()
-        server = LocalHTTPServer(port: port)
-
-        let manifest = buildManifest(bundleID: bundleID, appName: appName)
-
-        server?.addRoute("/manifest.plist") { _ in HTTPResponse(body: manifest, contentType: "text/xml") }
-        server?.addRoute("/app.ipa") { _ in
-            HTTPResponse(body: (try? Data(contentsOf: ipaURL)) ?? Data(), contentType: "application/octet-stream")
+    func install(ipaURL: URL, bundleID: String) {
+        do {
+            let port = 8765
+            server = try LocalHTTPServer(ipaURL: ipaURL, bundleID: bundleID, port: port)
+            let manifestURL = "http://127.0.0.1:\(port)/manifest.plist"
+            let installURL = "itms-services://?action=download-manifest&url=\(manifestURL)"
+            guard let url = URL(string: installURL.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) ?? installURL) else { return }
+            DispatchQueue.main.async {
+                UIApplication.shared.open(url)
+            }
+        } catch {
+            print("OTA install error: \(error)")
         }
-        server?.start()
-
-        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
-            let urlStr = "itms-services://?action=download-manifest&url=http://localhost:\(self.port)/manifest.plist"
-            if let url = URL(string: urlStr) { UIApplication.shared.open(url) }
-        }
-    }
-
-    private func buildManifest(bundleID: String, appName: String) -> Data {
-        """
-        <?xml version="1.0" encoding="UTF-8"?>
-        <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-        <plist version="1.0"><dict>
-            <key>items</key><array><dict>
-                <key>assets</key><array><dict>
-                    <key>kind</key><string>software-package</string>
-                    <key>url</key><string>http://localhost:\(port)/app.ipa</string>
-                </dict></array>
-                <key>metadata</key><dict>
-                    <key>bundle-identifier</key><string>\(bundleID)</string>
-                    <key>bundle-version</key><string>1.0</string>
-                    <key>kind</key><string>software</string>
-                    <key>title</key><string>\(appName)</string>
-                </dict>
-            </dict></array>
-        </dict></plist>
-        """.data(using: .utf8) ?? Data()
     }
 }
 
-struct HTTPResponse { let body: Data; let contentType: String }
-
 class LocalHTTPServer {
+    private var thread: Thread?
+    private var serverSocket: Int32 = -1
+    private let ipaURL: URL
+    private let bundleID: String
     private let port: Int
-    private var routes: [String: (String) -> HTTPResponse] = [:]
-    private var running = false
 
-    init(port: Int) { self.port = port }
-    func addRoute(_ path: String, handler: @escaping (String) -> HTTPResponse) { routes[path] = handler }
-    func start() { running = true; DispatchQueue.global(qos: .background).async { self.runLoop() } }
-    func stop() { running = false }
+    init(ipaURL: URL, bundleID: String, port: Int) throws {
+        self.ipaURL = ipaURL
+        self.bundleID = bundleID
+        self.port = port
 
-    private func runLoop() {
-        let fd = socket(AF_INET, SOCK_STREAM, 0)
-        guard fd >= 0 else { return }
-        defer { close(fd) }
+        serverSocket = socket(AF_INET, SOCK_STREAM, 0)
+        guard serverSocket >= 0 else { throw NSError(domain: "LocalHTTPServer", code: 1) }
+
         var opt: Int32 = 1
-        setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &opt, socklen_t(MemoryLayout<Int32>.size))
+        setsockopt(serverSocket, SOL_SOCKET, SO_REUSEADDR, &opt, socklen_t(MemoryLayout<Int32>.size))
+
         var addr = sockaddr_in()
         addr.sin_family = sa_family_t(AF_INET)
         addr.sin_port = in_port_t(port).bigEndian
         addr.sin_addr.s_addr = INADDR_ANY
-        let bound = withUnsafePointer(to: &addr) {
-            $0.withMemoryRebound(to: sockaddr.self, capacity: 1) { bind(fd, $0, socklen_t(MemoryLayout<sockaddr_in>.size)) }
+
+        let bindResult = withUnsafePointer(to: &addr) {
+            $0.withMemoryRebound(to: sockaddr.self, capacity: 1) {
+                bind(serverSocket, $0, socklen_t(MemoryLayout<sockaddr_in>.size))
+            }
         }
-        guard bound == 0 else { return }
-        listen(fd, 5)
-        while running {
-            let client = accept(fd, nil, nil)
-            guard client >= 0 else { continue }
-            handleClient(fd: client)
-        }
+        guard bindResult == 0 else { throw NSError(domain: "LocalHTTPServer", code: 2) }
+        listen(serverSocket, 5)
+
+        thread = Thread { [weak self] in self?.acceptLoop() }
+        thread?.start()
     }
 
-    private func handleClient(fd: Int32) {
-        defer { close(fd) }
-        var buf = [UInt8](repeating: 0, count: 4096)
-        let n = read(fd, &buf, buf.count)
-        guard n > 0 else { return }
-        let req = String(bytes: buf.prefix(n), encoding: .utf8) ?? ""
-        let path = req.components(separatedBy: " ").dropFirst().first ?? "/"
-        if let handler = routes[path] {
-            let res = handler(req)
-            let header = "HTTP/1.1 200 OK\r\nContent-Type: \(res.contentType)\r\nContent-Length: \(res.body.count)\r\nConnection: close\r\n\r\n"
-            _ = header.withCString { write(fd, $0, strlen($0)) }
-            res.body.withUnsafeBytes { _ = write(fd, $0.baseAddress!, res.body.count) }
-        } else {
-            _ = "HTTP/1.1 404 Not Found\r\nContent-Length: 0\r\n\r\n".withCString { write(fd, $0, strlen($0)) }
+    private func acceptLoop() {
+        var clientAddr = sockaddr_in()
+        var addrLen = socklen_t(MemoryLayout<sockaddr_in>.size)
+        for _ in 0..<10 {
+            let client = withUnsafeMutablePointer(to: &clientAddr) {
+                $0.withMemoryRebound(to: sockaddr.self, capacity: 1) {
+                    accept(serverSocket, $0, &addrLen)
+                }
+            }
+            guard client >= 0 else { break }
+            handleClient(client)
         }
+        close(serverSocket)
+    }
+
+    private func handleClient(_ client: Int32) {
+        var buf = [UInt8](repeating: 0, count: 4096)
+        read(client, &buf, buf.count)
+        let request = String(bytes: buf, encoding: .utf8) ?? ""
+
+        let response: Data
+        if request.contains("GET /manifest.plist") {
+            let manifest = buildManifest()
+            response = httpResponse(body: manifest, contentType: "text/xml")
+        } else if request.contains("GET /app.ipa") {
+            response = (try? Data(contentsOf: ipaURL)) ?? Data()
+            let header = "HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: \(response.count)\r\n\r\n"
+            let headerData = header.data(using: .utf8)!
+            headerData.withUnsafeBytes { write(client, $0.baseAddress, headerData.count) }
+            response.withUnsafeBytes { write(client, $0.baseAddress, response.count) }
+            close(client)
+            return
+        } else {
+            response = httpResponse(body: Data(), contentType: "text/plain")
+        }
+
+        response.withUnsafeBytes { write(client, $0.baseAddress, response.count) }
+        close(client)
+    }
+
+    private func buildManifest() -> Data {
+        let ipaSize = (try? ipaURL.resourceValues(forKeys: [.fileSizeKey]).fileSize) ?? 0
+        let xml = """
+        <?xml version="1.0" encoding="UTF-8"?>
+        <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+        <plist version="1.0">
+        <dict>
+            <key>items</key>
+            <array>
+                <dict>
+                    <key>assets</key>
+                    <array>
+                        <dict>
+                            <key>kind</key><string>software-package</string>
+                            <key>url</key><string>http://127.0.0.1:\(port)/app.ipa</string>
+                            <key>md5-size</key><integer>\(ipaSize)</integer>
+                        </dict>
+                    </array>
+                    <key>metadata</key>
+                    <dict>
+                        <key>bundle-identifier</key><string>\(bundleID)</string>
+                        <key>bundle-version</key><string>1.0</string>
+                        <key>kind</key><string>software</string>
+                        <key>title</key><string>App</string>
+                    </dict>
+                </dict>
+            </array>
+        </dict>
+        </plist>
+        """
+        return xml.data(using: .utf8) ?? Data()
+    }
+
+    private func httpResponse(body: Data, contentType: String) -> Data {
+        let header = "HTTP/1.1 200 OK\r\nContent-Type: \(contentType)\r\nContent-Length: \(body.count)\r\n\r\n"
+        var data = header.data(using: .utf8)!
+        data.append(body)
+        return data
     }
 }
